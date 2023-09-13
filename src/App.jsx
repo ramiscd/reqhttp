bfbcbb7b9a169c4a372de7ed10aea24958c2352c
Ramis Damasceno <ramis1998d@gmail.com>
2023-09-13 20:49:47 -0300

    Adicionando useEfect

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,21 @@ const url = "http://localhost:3000/products"
 function App() {
   const {products, setProducts} = useState([]);
   
-  //1 - resgatando dados 
-  useEffect(async)
+  //1 - resgatando dados.
+  useEffect(async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setProducts(data)
+  }, []);
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) =>(
+          <li key={product.id}> {product.name} - R$ {product.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
